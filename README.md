23d72e5e346e491f99944044bcf0da198babd1af
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-11-26 18:10:14 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# CRON Job Manager
+A class for managing cron jobs.
+
+## Installation
+Install the package with NPM:
+
+```
+npm install @donutteam/cron-job-manager
+```
+
+## License
+[MIT](https://github.com/donutteam/npm-cron-job-manager/blob/main/LICENSE.md)
---
+++ src/classes/CronJobManager.ts
@@ -0,0 +1,81 @@
+//
+// Imports
+//
+
+import cron from "node-cron";
+
+//
+// Class
+//
+
+export interface CronJob
+{
+	expression : string;
+
+	title : string;
+
+	runAtStartup : boolean;
+
+	callback : (scheduledCronJob : ScheduledCronJob) => Promise<void>;
+}
+
+export interface ScheduledCronJob extends CronJob
+{
+	scheduledTask : cron.ScheduledTask | null;
+
+	log : (message : string) => void;
+}
+
+/** A class that manages cron jobs. */
+export class CronJobManager
+{
+	/** All registered cron jobs. */
+	static scheduledCronJobs : ScheduledCronJob[] = [];
+
+	/** Executes all registered run-at-startup cron jobs. */
+	static executeAll()
+	{
+		for (const cronJob of this.scheduledCronJobs.filter(cronJob => cronJob.runAtStartup))
+		{
+			cronJob.callback(cronJob).then();
+		}
+	}
+
+	/** Registers the given cron job. */
+	static register(cronJob : CronJob) : ScheduledCronJob
+	{
+		console.log("[CronJobManager] Registered cron job: " + cronJob.title);
+
+		const scheduledCronJob : ScheduledCronJob =
+			{
+				...cronJob,
+
+				scheduledTask: null,
+
+				log: (message : string) =>
+				{
+					console.log("[CronJobManager] [" + cronJob.title + "] " + message);
+				},
+			};
+
+		scheduledCronJob.scheduledTask = cron.schedule(cronJob.expression, () =>
+		{
+			cronJob.callback(scheduledCronJob).then();
+		});
+
+		this.scheduledCronJobs.push(scheduledCronJob);
+
+		return scheduledCronJob;
+	}
+
+	static stopAll()
+	{
+		for (const cronJob of this.scheduledCronJobs)
+		{
+			if (cronJob.scheduledTask !== null)
+			{
+				cronJob.scheduledTask.stop();
+			}
+		}
+	}
+}
